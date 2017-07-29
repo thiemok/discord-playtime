5b4815676a79643c99ed607f31f2ffb79dd66736
thiemok <thiemok@users.noreply.github.com>
2017-07-29 18:46:39 +0200

    feat(Jest): Add tests for command userStats

--- app/__mocks__/database.js
+++ app/__mocks__/database.js
@@ -10,6 +10,7 @@ const __setMockData = (data) => {
 db.getTopPlayers = jest.fn(id => mockData.topPlayers);
 db.getTopGames = jest.fn(id => mockData.topGames);
 db.getTotalTimePlayed = jest.fn(id => mockData.totalTimeplayed);
+db.getGamesforPlayer = jest.fn(id => new Promise((res, rej) => res(mockData.gamesForPlayer)));
 
 db.__setMockData = __setMockData;
 
