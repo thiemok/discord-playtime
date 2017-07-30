1542fcf4edfd77777a0304f4aa3777806aab9f5c
thiemok <thiemok@users.noreply.github.com>
2017-07-30 03:32:41 +0200

    feat(Jest): Add tests for util functions

---
+++ app/util/__tests__/embedHelpers.test.js
@@ -0,0 +1,34 @@
+/* eslint-env jest */
+import embedHelpers from '../embedHelpers';
+import { RichEmbed } from 'discord.js';
+import MockDate from 'mockdate';
+import mockClientFactory from '../../../__mocks__/discordjs.client';
+import mockGuildMemberFactory from '../../../__mocks__/discordjs.guildMember';
+
+const serverID = 'test';
+const members = new Map([
+	[0, mockGuildMemberFactory(0)],
+]);
+const client = mockClientFactory(members);
+
+beforeAll(() => {
+	MockDate.set(1434319925275);
+});
+
+afterAll(() => {
+	MockDate.reset();
+});
+
+describe('EmbedHelpers', () => {
+	test('initCustomRichEmbed works correctly', () => {
+		const expectedEmbed = new RichEmbed()
+		.setColor(members.get(0).highestRole.color)
+		.setTimestamp()
+		.setFooter(
+			'Powered by discord-playtime',
+			'https://assets-cdn.github.com/favicon.ico'
+		);
+
+		expect(embedHelpers.initCustomRichEmbed(serverID, client)).toEqual(expectedEmbed);
+	});
+});
