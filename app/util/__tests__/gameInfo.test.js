5a867e999bbfd1ae9c6fbbb5ca1ee345636d72f7
thiemok <thiemok@users.noreply.github.com>
2017-07-30 11:16:30 +0200

    fix(Jest): Fix errors in gameInfo tests and add missing coverage to stringHelpers test

--- app/util/__tests__/gameInfo.test.js
+++ app/util/__tests__/gameInfo.test.js
@@ -10,56 +10,52 @@ beforeEach(() => {
 
 describe('GameInfo', () => {
 
-	describe('findGameURL', () => {
-		test('resolves on successful search with url', (done) => {
-			const mockResponse = { body: [{ url: 'https://mockgame.game' }] };
-			igdb.games.mockImplementationOnce(search => new Promise((res, rej) => res(mockResponse)));
+	test('findGameURL resolves on successful search with url', (done) => {
+		const mockResponse = { body: [{ url: 'https://mockgame.game' }] };
+		igdb.games.mockImplementationOnce(search => Promise.resolve(mockResponse));
 
-			gameInfo.findGameURL('mockgame')
-			.then((resp) => {
-				expect(resp).toEqual('https://mockgame.game');
-				expect(igdb.games).lastCalledWith({ search: 'mockgame', fields: 'url' });
-				done();
-			}).catch((e) => {
-				// Fail by default here
-				/* eslint-disable no-undef */
-				fail('Got error when there should not have been one:\n' + e);
-				/* eslint-enable no-undef */
-				done();
-			});
-
-			test('rejects on igdb error', () => {
-				igdb.games.mockImplementationOnce(search => new Promise((res, rej) => rej('Fail')));
-
-				expect(gameInfo.findGameURL('mockgame')).rejects.toBe('Fail');
-			});
+		gameInfo.findGameURL('mockgame')
+		.then((resp) => {
+			expect(resp).toEqual('https://mockgame.game');
+			expect(igdb.games).lastCalledWith({ search: 'mockgame', fields: 'url' });
+			done();
+		}).catch((e) => {
+			// Fail by default here
+			/* eslint-disable no-undef */
+			fail('Got error when there should not have been one:\n' + e);
+			/* eslint-enable no-undef */
+			done();
 		});
 	});
 
-	describe('findGameCover', () => {
-		test('resolves on successful search with url', (done) => {
-			const mockResponse = { body: [{ cover: { url: '//mockgame.game' } }] };
-			igdb.games.mockImplementationOnce(search => new Promise((res, rej) => res(mockResponse)));
+	test('findGameURL rejects on igdb error', () => {
+		igdb.games.mockImplementationOnce(search => Promise.reject('Fail'));
 
-			gameInfo.findGameCover('mockgame')
-			.then((resp) => {
-				expect(resp).toEqual('https://mockgame.game');
-				expect(igdb.games).lastCalledWith({ search: 'mockgame', fields: 'cover' });
-				done();
-			}).catch((e) => {
-				// Fail by default here
-				/* eslint-disable no-undef */
-				fail('Got error when there should not have been one:\n' + e);
-				/* eslint-enable no-undef */
-				done();
-			});
+		expect(gameInfo.findGameURL('mockgame')).rejects.toBe('Fail');
+	});
 
-			test('rejects on igdb error', () => {
-				igdb.games.mockImplementationOnce(search => new Promise((res, rej) => rej('Fail')));
+	test('findGameCover resolves on successful search with url', (done) => {
+		const mockResponse = { body: [{ cover: { url: '//mockgame.game' } }] };
+		igdb.games.mockImplementationOnce(search => Promise.resolve(mockResponse));
 
-				expect(gameInfo.findGameCover('mockgame')).rejects.toBe('Fail');
-			});
+		gameInfo.findGameCover('mockgame')
+		.then((resp) => {
+			expect(resp).toEqual('https://mockgame.game');
+			expect(igdb.games).lastCalledWith({ search: 'mockgame', fields: 'cover' });
+			done();
+		}).catch((e) => {
+			// Fail by default here
+			/* eslint-disable no-undef */
+			fail('Got error when there should not have been one:\n' + e);
+			/* eslint-enable no-undef */
+			done();
 		});
+	});
+
+	test('findGameCover rejects on igdb error', () => {
+		igdb.games.mockImplementationOnce(search => Promise.reject('Fail'));
+
+		expect(gameInfo.findGameCover('mockgame')).resolves.toBeNull();
 	});
 
 });
--- app/util/__tests__/stringHelpers.test.js
+++ app/util/__tests__/stringHelpers.test.js
@@ -5,30 +5,39 @@ import { findGameURL } from '../gameInfo';
 jest.mock('../gameInfo');
 
 describe('StringHelpers', () => {
-	test('buildTimeString works correctly', () => {
+
+	test('buildTimeString works correctly with d/h/min', () => {
 		const expectedTimeString = '*1d 1h 0min*';
 		expect(stringHelpers.buildTimeString(90010000)).toBe(expectedTimeString);
 	});
 
-	describe('buildRichGameString', () => {
-		test('works correctly for game found in db', () => {
-			const testGame = { _id: 'TestGame', total: 90010000 };
-			const testGameURL = 'https://' + testGame._id + '.game';
-			const expectedGameString = '**[' + testGame._id + ']('
-			+ testGameURL + ')**: '
-			+ stringHelpers.buildTimeString(testGame.total);
-			findGameURL.mockImplementationOnce(game => Promise.resolve(testGameURL));
+	test('buildTimeString works correctly with h/min', () => {
+		const expectedTimeString = '*2h 30min*';
+		expect(stringHelpers.buildTimeString(9001000)).toBe(expectedTimeString);
+	});
 
-			expect(stringHelpers.buildRichGameString(testGame)).resolves.toBe(expectedGameString);
-		});
+	test('buildTimeString works correctly with min', () => {
+		const expectedTimeString = '*15min*';
+		expect(stringHelpers.buildTimeString(900100)).toBe(expectedTimeString);
+	});
 
-		test('works correctly for game not found in db', () => {
-			const testGame = { _id: 'TestGame', total: 90010000 };
-			const expectedGameString = '**' + testGame._id + '**: '
-			+ stringHelpers.buildTimeString(testGame.total);
-			findGameURL.mockImplementationOnce(game => Promise.reject());
+	test('buildRichGameString works correctly for game found in db', () => {
+		const testGame = { _id: 'TestGame', total: 90010000 };
+		const testGameURL = 'https://' + testGame._id + '.game';
+		const expectedGameString = '**[' + testGame._id + ']('
+		+ testGameURL + ')**: '
+		+ stringHelpers.buildTimeString(testGame.total);
+		findGameURL.mockImplementationOnce(game => Promise.resolve(testGameURL));
 
-			expect(stringHelpers.buildRichGameString(testGame)).resolves.toBe(expectedGameString);
-		});
+		expect(stringHelpers.buildRichGameString(testGame)).resolves.toBe(expectedGameString);
+	});
+
+	test('buildRichGameString works correctly for game not found in db', () => {
+		const testGame = { _id: 'TestGame', total: 90010000 };
+		const expectedGameString = '**' + testGame._id + '**: '
+		+ stringHelpers.buildTimeString(testGame.total);
+		findGameURL.mockImplementationOnce(game => Promise.reject());
+
+		expect(stringHelpers.buildRichGameString(testGame)).resolves.toBe(expectedGameString);
 	});
 });
