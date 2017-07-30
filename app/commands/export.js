7e763f5cc025fd350fba957272264034f0b9dc02
thiemok <thiemok@users.noreply.github.com>
2017-07-30 02:37:10 +0200

    feat(Jest): Add tests for command matching and exportJSON command

--- app/commands/export.js
+++ app/commands/export.js
@@ -5,26 +5,26 @@
  * @return {Promise}              Resolves when the export has finished, with a sendable object
  */
 const exportJSON = (argv, context) => {
-	const { sender, serverID, db } = context;
+	const { member, serverID, db } = context;
 	const pResult = new Promise(function(resolve, reject) {
 		// Needs to be admin to export db
-		if (sender.permissions.hasPermission('ADMINISTRATOR')) {
+		if (member.permissions.hasPermission('ADMINISTRATOR')) {
 			// Export data
 			db.getAllDataForServer(serverID)
-			.then(function(_data) {
+			.then((data) => {
 				// Create buffer from string representation of data and send it
-				sender.sendFile(
-					Buffer.from(JSON.stringify(_data, null, '\t')),
+				member.sendFile(
+					Buffer.from(JSON.stringify(data, null, '\t')),
 					'export.JSON',
 					'Data export finished'
 				)
-				.then(function(_msg) {
+				.then(() => {
 					resolve("psst I'm sending you a private message");
-				}).catch(function(err) {
-					resolve('`' + err + '`');
+				}).catch((err) => {
+					resolve('`Error: ' + err + '`');
 				});
-			}).catch(function(err) {
-				resolve('`' + err + '`');
+			}).catch((err) => {
+				resolve('`Error: ' + err + '`');
 			});
 		} else {
 			resolve('`You have insufficient permissions, only Admins can export`');
