ae06411aa95d67e22338882398d600b1356432a2
thiemok <thiemok@users.noreply.github.com>
2017-07-31 20:55:52 +0200

    feat(Jest): Add tests for session updater

--- __mocks__/discordjs.guildMember.js
+++ __mocks__/discordjs.guildMember.js
@@ -2,15 +2,20 @@
 
 const mockGuildMemberFactory = (id) => {
 	return {
+		id,
 		displayName: 'mockGuildMember' + id,
 		user: {
 			avatarURL: 'https://testurl.com',
+			bot: false,
 		},
 		highestRole: {
 			color: 0,
 		},
 		permissions: {
 			hasPermission: jest.fn(() => true),
+		},
+		presence: {
+			status: 'online',
 		},
 		sendFile: jest.fn(() => new Promise((res, rej) => res())),
 	};
--- app/__mocks__/database.js
+++ app/__mocks__/database.js
@@ -5,7 +5,10 @@ let mockData = {};
 
 const __setMockData = (data) => {
 	mockData = Object.assign({}, data);
+	mockData.sessions = [];
 };
+
+const __getMockData = () => mockData;
 
 db.getTopPlayers = jest.fn(id => Promise.resolve(mockData.topPlayers));
 db.getTopGames = jest.fn(id => Promise.resolve(mockData.topGames));
@@ -13,7 +16,12 @@ db.getTotalTimePlayed = jest.fn(id => Promise.resolve(mockData.totalTimeplayed))
 db.getGamesforPlayer = jest.fn(id => Promise.resolve(mockData.gamesForPlayer));
 db.getGame = jest.fn((id, name) => Promise.resolve(mockData.game));
 db.getAllDataForServer = jest.fn((id, name) => Promise.resolve(mockData));
+db.insertSession = jest.fn((session, callback) => {
+	mockData.sessions.push(session);
+	callback();
+});
 
 db.__setMockData = __setMockData;
+db.__getMockData = __getMockData;
 
 export default db;
