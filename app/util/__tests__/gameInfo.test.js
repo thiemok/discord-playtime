3e5cd37312bdb1ab209e58fde9ebeff42d75f893
thiemok <thiemok@users.noreply.github.com>
2017-08-04 11:30:01 +0200

    fix(Tests): fix broken test for gameInfo

--- app/util/__tests__/gameInfo.test.js
+++ app/util/__tests__/gameInfo.test.js
@@ -1,33 +1,23 @@
 /* eslint-env jest */
 import gameInfo from '../gameInfo';
-import igdb from 'igdb-api-node';
 
-// let igdb = require('igdb-api-node');
-
+const mockIgdb = {
+	games: jest.fn(),
+};
 jest.mock('igdb-api-node', () => {
-	return () => ({
-		games: jest.fn(),
-	});
+	return () => mockIgdb;
 });
-
-// igdb = {
-// 	default: () => ({
-// 		games: jest.fn(),
-// 	}),
-// };
-
-const igdbClient = igdb();
 
 describe('GameInfo', () => {
 
 	test('findGameURL resolves on successful search with url', (done) => {
 		const mockResponse = { body: [{ url: 'https://mockgame.game' }] };
-		igdbClient.games.mockImplementationOnce(search => Promise.resolve(mockResponse));
+		mockIgdb.games.mockImplementationOnce(search => Promise.resolve(mockResponse));
 
 		gameInfo.findGameURL('mockgame')
 			.then((resp) => {
 				expect(resp).toEqual('https://mockgame.game');
-				expect(igdbClient.games).lastCalledWith({ search: 'mockgame', fields: 'url' });
+				expect(mockIgdb.games).lastCalledWith({ search: 'mockgame', fields: 'url' });
 				done();
 			}).catch((e) => {
 			// Fail by default here
@@ -39,19 +29,19 @@ describe('GameInfo', () => {
 	});
 
 	test('findGameURL rejects on igdb error', () => {
-		igdbClient.games.mockImplementationOnce(search => Promise.reject('Fail'));
+		mockIgdb.games.mockImplementationOnce(search => Promise.reject('Fail'));
 
 		expect(gameInfo.findGameURL('mockgame')).rejects.toBe('Fail');
 	});
 
 	test('findGameCover resolves on successful search with url', (done) => {
 		const mockResponse = { body: [{ cover: { url: '//mockgame.game' } }] };
-		igdbClient.games.mockImplementationOnce(search => Promise.resolve(mockResponse));
+		mockIgdb.games.mockImplementationOnce(search => Promise.resolve(mockResponse));
 
 		gameInfo.findGameCover('mockgame')
 			.then((resp) => {
 				expect(resp).toEqual('https://mockgame.game');
-				expect(igdbClient.games).lastCalledWith({ search: 'mockgame', fields: 'cover' });
+				expect(mockIgdb.games).lastCalledWith({ search: 'mockgame', fields: 'cover' });
 				done();
 			}).catch((e) => {
 			// Fail by default here
@@ -63,7 +53,7 @@ describe('GameInfo', () => {
 	});
 
 	test('findGameCover rejects on igdb error', () => {
-		igdbClient.games.mockImplementationOnce(search => Promise.reject('Fail'));
+		mockIgdb.games.mockImplementationOnce(search => Promise.reject('Fail'));
 
 		expect(gameInfo.findGameCover('mockgame')).resolves.toBeNull();
 	});
